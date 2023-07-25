8cb6c49f12d10647e3d0808402408624cae56dd5
Rubel Rudra <rubelrudra27@gmail.com>
2023-07-26 01:49:21 +0600

    Complete & Solve reverse string problem

--- reverseString.js
+++ reverseString.js
@@ -1,3 +1,15 @@
 function reverseString(string){
-  
+  if(string==null || typeof string!=='string'){
+    return;
+  }
+
+  let reverseStr='';
+  for(let i=string.length-1; i>=0; i--){
+    reverseStr+=string[i];
+  }
+  return reverseStr;
 }
+
+const myString='Rubel Rudra';
+const result=reverseString(myString);
+console.log(result);
