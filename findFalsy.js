59771f025e590361e8a2d02e4834f1a1e23dc35d
Rubel Rudra <rubelrudra27@gmail.com>
2024-01-23 12:39:57 +0600

    find & delete falsy value from object

--- findFalsy.js
+++ findFalsy.js
@@ -1,3 +1,4 @@
+// find & remove falsy value from array
 const mixedArr=["lws", undefined,"math", false,"", "apple", 40,
 "iphone", true, "thanks all", NaN];
 
@@ -15,3 +16,29 @@ const trueArray=mixedArr.filter(Boolean);
 
 console.log(trueArray);
 
+// find & delete falsy value from object
+
+const obj={ 
+  a:"lws", 
+  b:undefined, 
+  c:"math", 
+  d:false, 
+  e:"", 
+  f:"apple", 
+  g:40, 
+  h:"iphone",
+  i:true,
+  j:"thanks all",
+  k:NaN
+}
+
+function truthyObj(obj){
+  for(let key in obj) {
+    if(!obj[key]){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
+
+console.log(truthyObj(obj));
